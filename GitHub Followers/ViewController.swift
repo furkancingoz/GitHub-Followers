18cf87acaa11ffd51deb0f8f5fcedb776180dc83
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-27 02:15:40 +0300

    Setup the project
    Removing the storyboard
    SceneDelegate
    App Icon
    Light/Dark Mode assets

--- GitHub Followers/ViewController.swift
+++ GitHub Followers/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .blue
   }
 
 
 }
 
+
+
+
+
+////MARK: - Preview
+//#Preview {
+//  ViewControllerViewRepresentable()
+//}
+//struct ViewControllerViewRepresentable: UIViewControllerRepresentable {
+//  func makeUIViewController(context: Context) -> ViewController {
+//    // PlayerDetailVC'nin örneğini oluştur.
+//    return ViewController()
+//  }
+//
+//  func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+//    // Bu fonksiyon, view controller'ı güncellemek için kullanılır.
+//  }
+//}
