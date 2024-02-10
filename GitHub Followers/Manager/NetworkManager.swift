08aaa90e3c74878c21e65aae2b1f726912f30c6f
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-02-11 00:49:46 +0300

    Result Type Refactor

--- GitHub Followers/Manager/NetworkManager.swift
+++ GitHub Followers/Manager/NetworkManager.swift
@@ -15,34 +15,33 @@
       private init() {}
 
 
-
-      func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void) {
+      func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
           let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
 
           guard let url = URL(string: endpoint) else {
-            completed(nil, .invalidUsername)
+            completed(.failure(.invalidUsername))
               return
           }
 
           let task = URLSession.shared.dataTask(with: url) { data, response, error in
 
               if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
                   return
               }
 
             guard let response = response as? HTTPURLResponse else {
-              completed(nil, .invalidResponse)
+              completed(.failure(.invalidResponse))
                 return
             }
             print("Status Code: \(response.statusCode)")
             guard response.statusCode == 200 else {
-              completed(nil, .invalidResponse)
+              completed(.failure(.invalidResponse))
                 return
             }
 
               guard let data = data else {
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                   return
               }
 
@@ -50,10 +49,9 @@
                   let decoder = JSONDecoder()
                   decoder.keyDecodingStrategy = .convertFromSnakeCase
                   let followers = try decoder.decode([Follower].self, from: data)
-                  completed(followers, nil)
+                completed(.success(followers))
               } catch {
-                completed(nil, .invalidData)
-                print(error.localizedDescription)
+                completed(.failure(.invalidResponse))
               }
           }
 
--- GitHub Followers/Screen/FollowersListVC.swift
+++ GitHub Followers/Screen/FollowersListVC.swift
@@ -8,25 +8,26 @@
 import UIKit
 
 class FollowersListVC: UIViewController {
-
+  
   var username: String!
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
-    NetworkManager.shared.getFollowers(for: username, page: 1) { flwrs, errorMessage in
-      guard let followers = flwrs else {
-        self.presentGFAlertOnMainThread(title: "Bad things happend", message: errorMessage!.rawValue, buttonTitle: "OK")
-        return
+    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+      
+      switch result {
+      case .success(let followers):
+        print(followers)
+        
+      case.failure(let error):
+        self.presentGFAlertOnMainThread(title: "Bad things happend", message: error.rawValue, buttonTitle: "OK")
       }
-
-      print("Followers count \(followers.count)")
-      print(followers)
     }
   }
-
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     navigationController?.setNavigationBarHidden(false, animated: true)
