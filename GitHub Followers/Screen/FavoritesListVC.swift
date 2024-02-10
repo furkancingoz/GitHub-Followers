d77434c9702475990ce38b45a19730a5fded8c83
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-02-11 01:25:29 +0300

    create customs cell

--- GitHub Followers/Screen/FavoritesListVC.swift
+++ GitHub Followers/Screen/FavoritesListVC.swift
@@ -11,8 +11,13 @@ class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      view.backgroundColor = .green
+      view.backgroundColor = .white
 
     }
 
 }
+
+
+
+
+
